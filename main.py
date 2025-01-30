7c3d64b1369c41b17eebfba76ff85e0f82eadcba
Carl <cmillardv77@gmail.com>
2025-01-30 00:13:00 -0500

    Text Options Branch - Text Options Finished - Logo Window Begin

--- main.py
+++ main.py
@@ -28,15 +28,12 @@ main_window.grid_columnconfigure(index=2, weight=1)
 # Adjusts canvas to size of image, then displays image - Connected to "Upload" button
 # Function will close other open windows when called
 def upload_image(photo_canvas):
-    if window_dict["watermark_window"] or window_dict["text_window"]:
+    if window_dict["watermark_window"]:
         window_dict["watermark_window"].destroy()
+    if window_dict["text_window"]:
         window_dict["text_window"].destroy()
-    if window_dict["text_window"] or window_dict["text_window"]:
-        window_dict["watermark_window"].destroy()
-        window_dict["text_window"].destroy()
-    if window_dict["logo_window"] or window_dict["text_window"]:
-        window_dict["watermark_window"].destroy()
-        window_dict["text_window"].destroy()
+    if window_dict["logo_window"]:
+        window_dict["logo_window"].destroy()
     uploaded_image_path = tk.filedialog.askopenfilename()
     if uploaded_image_path:
         file_type = magic.from_file(uploaded_image_path, mime=True)
@@ -49,8 +46,9 @@ def upload_image(photo_canvas):
             photo_canvas.pillow_image = image
 
             photo = ImageTk.PhotoImage(image)
-            photo_canvas.image = photo
-            photo_canvas.create_image(2, 2, image=photo, anchor="nw")
+            photo_canvas.original_image = photo
+            photo_canvas.delete(photo_canvas.image_id)
+            photo_canvas.image_id = photo_canvas.create_image(2, 2, image=photo, anchor="nw")
             watermark_options()
         else:
             print("That's not a valid file format.")
@@ -59,15 +57,12 @@ def upload_image(photo_canvas):
 # Same as upload_image function, but works with drag-and-drop
 # Function will close other open windows when called
 def on_drop(event, photo_canvas):
-    if window_dict["watermark_window"] or window_dict["text_window"]:
+    if window_dict["watermark_window"]:
         window_dict["watermark_window"].destroy()
+    if window_dict["text_window"]:
         window_dict["text_window"].destroy()
-    if window_dict["text_window"] or window_dict["text_window"]:
-        window_dict["watermark_window"].destroy()
-        window_dict["text_window"].destroy()
-    if window_dict["logo_window"] or window_dict["text_window"]:
-        window_dict["watermark_window"].destroy()
-        window_dict["text_window"].destroy()
+    if window_dict["logo_window"]:
+        window_dict["logo_window"].destroy()
     file_path = event.data.strip("{}")
     if file_path:
         file_type = magic.from_file(file_path, mime=True)
@@ -80,8 +75,9 @@ def on_drop(event, photo_canvas):
             photo_canvas.pillow_image = image
 
             photo = ImageTk.PhotoImage(image)
-            photo_canvas.image = photo
-            photo_canvas.create_image(2, 2, image=photo, anchor="nw")
+            photo_canvas.original_image = photo
+            photo_canvas.delete(photo_canvas.image_id)
+            photo_canvas.image_id = photo_canvas.create_image(2, 2, image=photo, anchor="nw")
             watermark_options()
         else:
             print("That's not a valid file format.")
@@ -173,6 +169,10 @@ def text_options():
     opacity_slider.set(255)
     opacity_label.grid(row=11, column=0, sticky="w", padx=10)
     opacity_slider.grid(row=12, column=0, sticky="w", padx=10)
+    save_image_button = tk.Button(text_window, text="Save Image", command=save_watermarked_image)
+    save_image_button.grid(row=13, column=0, sticky="w", padx=10, pady=10)
+    undo_changes_button = tk.Button(text_window, text="Undo Changes", command=undo_changes)
+    undo_changes_button.grid(row=13, column=1, sticky="w", padx=10, pady=10)
 
     # Stores the widgets as values, so they can be passed to add_text and retrieved via .get()
     # Button widget values aren't passed via this dictionary
@@ -210,6 +210,7 @@ def add_text(text_options, current_color, fonts, font_display_names):
     font_size = int(text_options["font_size"].get().strip("px"))
     angle = int(text_options["angle"].get())
     opacity = text_options["opacity"].get()
+    photo_canvas.opacity = opacity
     text_color = (current_color[0], current_color[1], current_color[2], opacity)
     font_index = font_display_names.index(font)
     font_file = ImageFont.truetype(f'fonts/{fonts[font_index]}', size=font_size)
@@ -224,9 +225,12 @@ def add_text(text_options, current_color, fonts, font_display_names):
         draw.text(xy=(new_image.width / 2, new_image.height / 2), text=text, font=font_file, fill=text_color,
                   anchor="mm")
         rotated_image = apply_angle(new_image, angle)
+        photo_canvas.watermark_pillow = rotated_image
         updated_image = ImageTk.PhotoImage(rotated_image)
-        photo_canvas.text_image = updated_image
-        photo_canvas.image_id = photo_canvas.create_image(
+        photo_canvas.watermark_image = updated_image
+        photo_canvas.watermark_x = x + ((font_size * .5) * (len(text) / 2))
+        photo_canvas.watermark_y = (photo_canvas.winfo_height() - y)
+        photo_canvas.watermark_id = photo_canvas.create_image(
             (x + ((font_size * .5) * (len(text) / 2)), (photo_canvas.winfo_height() - y)), image=updated_image)
         create_bounding_box()
 
@@ -241,9 +245,12 @@ def add_text(text_options, current_color, fonts, font_display_names):
             for x in range(0, tile_width, font_size * 5):
                 draw.text(xy=(x, y), text=text, font=font_file, fill=text_color, anchor="mm")
         rotated_image = apply_angle(new_image, angle)
+        photo_canvas.watermark_pillow = rotated_image
         updated_image = ImageTk.PhotoImage(rotated_image)
-        photo_canvas.text_image = updated_image
-        photo_canvas.image_id = photo_canvas.create_image(
+        photo_canvas.watermark_image = updated_image
+        photo_canvas.watermark_x = (photo_canvas.winfo_width() / 2) - 1
+        photo_canvas.watermark_y = (photo_canvas.winfo_height() / 2) - 1
+        photo_canvas.watermark_id = photo_canvas.create_image(
             photo_canvas.winfo_width() / 2, photo_canvas.winfo_height() / 2, image=updated_image, anchor="center")
         create_bounding_box()
     elif tiling == "diamond":
@@ -268,9 +275,12 @@ def add_text(text_options, current_color, fonts, font_display_names):
                 slide_line = True
 
         rotated_image = apply_angle(new_image, angle)
+        photo_canvas.watermark_pillow = rotated_image
         updated_image = ImageTk.PhotoImage(rotated_image)
-        photo_canvas.text_image = updated_image
-        photo_canvas.image_id = photo_canvas.create_image(
+        photo_canvas.watermark_image = updated_image
+        photo_canvas.watermark_x = (photo_canvas.winfo_width() / 2) - 1
+        photo_canvas.watermark_y = (photo_canvas.winfo_height() / 2) - 1
+        photo_canvas.watermark_id = photo_canvas.create_image(
             photo_canvas.winfo_width() / 2, photo_canvas.winfo_height() / 2, image=updated_image, anchor="center")
         create_bounding_box()
 
@@ -309,23 +319,45 @@ def create_bounding_box():
 
 # Recognizes when user clicks their mouse within the bounding box and holds the button down
 def clicked_text(event):
-    x = event.x
-    y = event.y
+    photo_canvas.watermark_x = event.x
+    photo_canvas.watermark_y = event.x
     if photo_canvas.bbox_id:
         bbox = photo_canvas.bbox(photo_canvas.image_id)
         x1, y1, x2, y2 = bbox
-        if x >= x1 and x <= x2 and y >= y1 and y <= y2:
+        if event.x >= x1 and event.x <= x2 and event.y >= y1 and event.y <= y2:
             photo_canvas.tag_bind(photo_canvas.bbox_id, "<ButtonRelease-1>", move_clicked_text)
 
 # Moves the text to the new space where the user released their mouse button
 def move_clicked_text(event):
-    x = event.x
-    y = event.y
-    new_text_image = photo_canvas.text_image
-    photo_canvas.delete(photo_canvas.image_id)
-    photo_canvas.image_id = photo_canvas.create_image(x, y, image=new_text_image)
+    photo_canvas.watermark_x = event.x
+    photo_canvas.watermark_y = event.y
+    new_watermark_image = photo_canvas.watermark_image
+    photo_canvas.delete(photo_canvas.watermark_id)
+    photo_canvas.watermark_id = photo_canvas.create_image(event.x, event.y, image=new_watermark_image)
     create_bounding_box()
     window_dict["text_window"].lift()
+
+# Undo Changes
+def undo_changes():
+    photo_canvas.delete(photo_canvas.watermark_id)
+
+
+# Save watermarked image
+def save_watermarked_image():
+    background = photo_canvas.pillow_image.convert("RGBA")
+    watermarked_image = background.copy().convert("RGBA")
+    watermark = photo_canvas.watermark_pillow
+    x = int(photo_canvas.watermark_x)
+    y = int(photo_canvas.watermark_y)
+    watermarked_image.paste(watermark,
+                            ((x - watermark.width // 2) - 1, (y - watermark.height // 2) - 1), watermark)
+    save_path = filedialog.asksaveasfilename(defaultextension=".png", filetypes=[(".png", "*.png"),
+                                                                                 (".jpg", "*.jpg*"),
+                                                                                 (".jpeg", "*.jpeg")])
+
+    watermarked_image.save(save_path)
+
+
 
 
 # Main App - Components
@@ -335,9 +367,9 @@ landscape_width, landscape_height = landscape_image.size
 landscape_photo = ImageTk.PhotoImage(landscape_image)
 drag_label = tk.Label(text="Drag your image", font=("Arial", 18))
 or_label = tk.Label(text="Or", font=("Arial", 12))
-photo_canvas = tk.Canvas(bg="black", width=landscape_width, height=landscape_height)
+photo_canvas = tk.Canvas(bg=None, width=landscape_width, height=landscape_height)
 upload_button = tk.Button(text="Upload", font=("Arial", 14), command=lambda: upload_image(photo_canvas))
-photo_canvas.image_id = photo_canvas.create_image(2, 2, image=landscape_photo, anchor="nw")
+photo_canvas.image_id = photo_canvas.create_image(photo_canvas.winfo_width() / 2, photo_canvas.winfo_height() / 2, image=landscape_photo, anchor="nw")
 photo_canvas.bbox_id = None
 photo_canvas.copyright = False
 
